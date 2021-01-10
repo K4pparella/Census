2353bbe2f3e4203bb85b05a5d1d58ab8ee2117cf
gamehunt <mt.demakov@gmail.com>
2021-01-10 15:52:16 +0300

    Little example plugin improvement

--- ExamplePlugin/ExamplePlugin.cs
+++ ExamplePlugin/ExamplePlugin.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
-namespace ExamplePlugin
+﻿namespace ExamplePlugin
 {
+    using System;
+    using CensusAPI.Features;
     public class ExamplePlugin : CensusAPI.Features.Plugin<ExampleConfig>
     {
         public override string Name => "ExamplePlugin";
@@ -10,14 +10,18 @@ namespace ExamplePlugin
         public override Version Version => new Version(1, 0, 0);
 
         public override string Author => "Census Team";
+        public static ExamplePlugin Instance { get; private set; }
 
         public override void Disable()
         {
+            Instance = null;
         }
 
         public override void Enable()
         {
+            Instance = this;
             CensusCore.CensusCore.InjectEvents();
+            Log.Info($"Parsed value: {Config.SomeValue}");
         }
     }
 }
